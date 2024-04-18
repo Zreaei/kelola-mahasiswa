0acb02b101f343148473c0200cd48bd41ac6037b
melany naila <nailamelany.16@upi.edu>
2024-04-18 23:22:06 +0700

    hapus mahasiswa

---
+++ deletM.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <malloc.h>
+using namespace std;
+
+void deleteNim (string targetNim){
+    while (head != NULL) {
+        if (current->nim == targetNim) { // mencari nim yang sesuai
+            if (current->prev != NULL) //pengecekkan letak node
+            {
+                current->prev->next = current->next;
+            }
+            if (current->next != NULL)
+            {
+                current->next->prev = current->prev;
+            } else {
+                tail = current->prev;
+            }
+            delete current; // menghapus nim
+            cout << "Nim " << targetNim << " telah dihapus" << endl;
+            return;
+        }
+    } cout << "Nim " << targetNim << " tidak ditemukan." << endl;
+}
+
+//
+cout << "Hapus Nim" << endl;
+string nimHapus;
+cout << "Masukkan nim yang ingin dihapus: ";
+cin >> nimHapus; 
+deletenim(nimHapus);
