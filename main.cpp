0f6f2c137bd89a5f81dc9f41fde660d1f7702343
Zreaei <zenarthxdgt@gmail.com>
2024-04-18 13:41:02 +0700

    First Commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void update(){
+    
+}
+
+int main(){
+    int pilihan;
+    cout << "========================================" << endl;
+    cout << "===== SISTEM KELOLA DATA MAHASISWA =====" << endl;
+    cout << "========================================" << endl;
+    cout << endl;
+    cout << "MENU OPERASI :" << endl;
+    cout << endl;
+    cout << "1. Cari Data Mahasiswa" << endl;
+    cout << "2. Tambah Data Mahasiswa" << endl;
+    cout << "3. Ubah Data Mahasiswa" << endl;
+    cout << "4. Hapus Data Mahasiswa" << endl;
+    cout << endl;
+    cout << "Masukan Menu : "; cin >> pilihan;
+}
