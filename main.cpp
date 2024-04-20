e55a64d93dc05934757719268c4ec14ac635b451
InIRaden <137896659+InIRaden@users.noreply.github.com>
2024-04-20 20:01:20 +0700

    Add files via upload
    eca udah melakukan update project

--- main.cpp
+++ main.cpp
@@ -2,8 +2,27 @@
 #include <stdlib.h>
 using namespace std;
 
-int main(){
-    int pilihan;
+struct Mahasiswa {
+    string nama;
+    int nim;
+    Mahasiswa* next;
+};
+
+void cariDataMahasiswa(Mahasiswa* head, int targetNIM) {
+    Mahasiswa* current = head;
+    while (current != NULL) {
+        if (current->nim == targetNIM) {
+            cout << "Data Mahasiswa ditemukan!" << endl;
+            cout << "NIM: " << current->nim << endl;
+            cout << "Nama: " << current->nama << endl;
+            return;
+        }
+        current = current->next;
+    }
+    cout << "Data Mahasiswa tidak ditemukan." << endl;
+}
+
+void tampilkanMenu() {
     system("cls");
     cout << "========================================" << endl;
     cout << "===== SISTEM KELOLA DATA MAHASISWA =====" << endl;
@@ -16,11 +35,29 @@ int main(){
     cout << "3. Ubah Data Mahasiswa" << endl;
     cout << "4. Hapus Data Mahasiswa" << endl;
     cout << endl;
+}
+
+int main() {
+    Mahasiswa* head = new Mahasiswa();
+    head->nim = 12345;
+    head->nama = "Raden";
+    
+    Mahasiswa* node1 = new Mahasiswa();
+    node1->nim = 67890;
+    node1->nama = "Zem";
+    
+    head->next = node1;
+    node1->next = NULL;
+
+    int pilihan;
+    tampilkanMenu();
     cout << "Masukan Menu : "; cin >> pilihan;
 
     if (pilihan == 1){
-        system("cls");
-        cout << "1 Done" << endl;
+        int targetNIM;
+        cout << "Masukkan NIM yang ingin dicari: ";
+        cin >> targetNIM;
+        cariDataMahasiswa(head, targetNIM);
     } else if (pilihan == 2){
         system("cls");
         cout << "2 Done" << endl;
@@ -34,4 +71,6 @@ int main(){
         system("cls");
         cout << "Masukan pilihan sesuai dengan opsi!" << endl;
     }
+
+    return 0;
 }
